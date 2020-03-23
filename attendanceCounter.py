4cdf7b3b576f87937bbd1c765e04cbb81f9bb7e0
Shezz7 <shehzaad.saifulla@tink.se>
2020-03-23 15:43:31 +0100

    fix(gam-events-stats): remove resource email

--- attendanceCounter.py
+++ attendanceCounter.py
@@ -1,14 +1,17 @@
+"""gets emails of the attendees and counts their attendance"""
 import collections
+import re
 
-def countAttendance(cal_objs,response):
-    i=0
-    email_list=[]
+
+def count_attendance(cal_objs, response):
+    email_list = []
 
     for each_record in cal_objs:
         for event_details in each_record['event']['attendees']:
-            if event_details['responseStatus']==response:
-                email_list.append(event_details['email'])                   # list of all emails matched
+            if ((event_details['responseStatus'] == response)
+               and not (re.match(".*@resource.calendar.google.com", event_details['email']))):
+                email_list.append(event_details['email'])
 
-    accepted_freq=collections.Counter(email_list)                           # Counter object of emails and frequencies
-    accepted_freq = [list(i) for i in accepted_freq.items()]                # Convert counter to list
+    accepted_freq = collections.Counter(email_list)
+    accepted_freq = [list(i) for i in accepted_freq.items()]
     print(accepted_freq)
--- gamcalendar.py
+++ gamcalendar.py
@@ -1,24 +1,31 @@
-import os
+"""gets the list of attendees of a recurring event"""
+
 import json
 import subprocess
-import collections
-from attendanceCounter import countAttendance
 import sys
+from attendancecounter import count_attendance
 
-email = sys.argv[1]
-event_id = sys.argv[2]
-response_answer = sys.argv[3]
 
-process = subprocess.run(['gam','calendar',email,'info','event','id',event_id,'maxinstances','0','fields','attendees','formatjson'
- ],
-                          stdout=subprocess.PIPE,
-                          universal_newlines=True)
-# collection of multiple JSON objects
-json_collection=str(process.stdout)
+def main():
+    email = sys.argv[1]
+    event_id = sys.argv[2]
+    response_answer = sys.argv[3]
 
-calendar_list=[]
-# parse JSON line by line
-for line in json_collection.splitlines():
-      calendar_list.append(json.loads(line))
+    process = subprocess.run(['gam', 'calendar', email, 'info', ' event', 'id', event_id,
+                              'maxinstances', '0', 'fields', 'attendees', 'formatjson'],
+                             stdout=subprocess.PIPE,
+                             universal_newlines=True,
+                             check=False)
 
-countAttendance(calendar_list,response_answer)
+    json_objects = str(process.stdout)
+
+    calendar_list = []
+    # parse JSON line by line
+    for line in json_objects.splitlines():
+        calendar_list.append(json.loads(line))
+
+    count_attendance(calendar_list, response_answer)
+
+
+if __name__ == '__main__':
+    main()
